d5dd0cb5f77b67f87b518b084dae7ba74e067325
Paulo de Almeida <paulodealmeida@gmail.com>
2016-04-20 16:55:15 -0300

    criada migration de produtos

---
+++ database/migrations/2016_04_20_195115_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->integer('products_category_id')->unsigned();
+            $table->foreign('products_category_id')->references('id')->on('products_categories');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+
+}
